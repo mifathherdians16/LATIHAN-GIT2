1817f186518df3bc49575bcbb3724be2693703a5
miftahherdians16 <miftahherdians16@gmail.com>
2021-09-22 21:43:16 +0700

    menambahkan index.html,about.html,contact.html,contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+		public function index()
+		{
+			echo "<h1>Perkenalkan</h1>";
+			echo "Nama saya Miftah Herdiansyah saya tinggal di Kp. Cisasah Bl daerah Ciapus hobby saya bermain bola";
+		}
+}
